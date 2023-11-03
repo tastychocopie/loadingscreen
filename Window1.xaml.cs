64d4b5c5b0538874fdef86fce4413a895a458c8f
tastychocopie <103076763+tastychocopie@users.noreply.github.com>
2023-11-03 14:01:39 +0300

    Add files via upload

---
+++ Window1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        DispatcherTimer timer;
+        public Window1()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) };
+            timer.Tick += Timer_tick;
+            timer.Start();
+        }
+        private void Timer_tick(object sender, object e)
+        {
+            load.Value++;
+            if (load.Value == 4)
+            {
+                timer.Stop();
+
+            }
+        }
+    }
+}
